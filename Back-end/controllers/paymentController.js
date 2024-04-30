333951a9d7dc66d151b4f5388c695af79ca840a6
Uday Singamala <143046094+Udaykir02@users.noreply.github.com>
2024-04-30 13:22:35 -0500

    Payment Controller Updated with Stripe

--- Back-end/controllers/paymentController.js
+++ Back-end/controllers/paymentController.js
@@ -1,13 +1,29 @@
-const Payment = require('../models/Payment');
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+// This example sets up an endpoint using the Express framework.
+// Watch this video to get started: https://youtu.be/rPR2aJ6XnAc.
 
-module.exports = {
-  addPayment: async (paymentData) => {
-    try {
-      return await Payment.insertMany(paymentData);
-    } catch (error) {
-      console.error("Error adding payments:", error);
-      throw new Error('Failed to add payments to the database');
-    }
-  },
-  // Other payment-related controller functions...
-};
+app.post('/payment-sheet', async (req, res) => {
+  // Use an existing Customer ID if this is a returning customer.
+  const customer = await stripe.customers.create();
+  const ephemeralKey = await stripe.ephemeralKeys.create(
+    {customer: customer.id},
+    {apiVersion: '2024-04-10'}
+  );
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1099,
+    currency: 'eur',
+    customer: customer.id,
+    // In the latest version of the API, specifying the `automatic_payment_methods` parameter
+    // is optional because Stripe enables its functionality by default.
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+  res.json({
+    paymentIntent: paymentIntent.client_secret,
+    ephemeralKey: ephemeralKey.secret,
+    customer: customer.id,
+    publishableKey: 'pk_test_TYooMQauvdEDq54NiTphI7jx'
+  });
+});
