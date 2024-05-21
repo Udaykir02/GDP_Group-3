ade6b8b1477c94e85a76f6224299d6cafbebe5da
varunrachakatla <vrachakatla@us.maxnerva.com>
2024-05-21 11:33:53 -0500

    new changes

--- Back-end/routes/orderRoutes.js
+++ Back-end/routes/orderRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { createOrder, getAllOrders, getOrder, updateOrder, deleteOrder, getOrderByVendorId } = require('../controllers/orderController');
+const { createOrder, getAllOrders, getOrder, updateOrder, deleteOrder, getOrderByVendorId, deleteAll } = require('../controllers/orderController');
 
 // Route for adding orders
 router.post('/create', createOrder);
 router.post('/orders', getAllOrders);
 router.post('/get', getOrder);
 router.post('/update', updateOrder);
-router.post('/delete', deleteOrder);
+router.post('/delete', deleteAll);
 router.post('/getOrderById',getOrderByVendorId)
 // Define other routes for orders as needed
 
--- Storegrab/src/Containers/Order/OrderContainer.tsx
+++ Storegrab/src/Containers/Order/OrderContainer.tsx
@@ -10,30 +10,34 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getOrders, placeOrders } from '../../../actions/userActions';
 import createToBeImplementedStyle from "../Order/OrderContainerStyle";
 import { getVendorRequestByID } from '../../../actions/vendorActions';
+import LoadingSpinner from '../../Components/shared/LoadingSpinner';
 
 const OrderContainer: React.FC = () => {
   const [isModalVisible, setIsModalVisible] = useState(true);
   const styles = createToBeImplementedStyle();
   const dispatch = useDispatch();
   const { user, vendorAdmin, token } = useSelector((state: any) => state.auth)
-  const { orders } = useSelector((state: any) => state.order)
+  const { orders, loading } = useSelector((state: any) => state.order)
   const { selectedVendor } = useSelector((state: any) => state.vendor)
   useEffect(() => {
     if (vendorAdmin) {
-      if(selectedVendor && selectedVendor?.vendorId)
-      dispatch(getVendorRequestByID(selectedVendor?.vendorId,token))
-
+      if (selectedVendor && selectedVendor?.vendorId)
+        dispatch(getVendorRequestByID(selectedVendor?.vendorId, token))
     } else {
       console.log('user', user?.userId ? user.userId : '')
       dispatch(getOrders(user?.userId ? user.userId : '', token))
     }
   }, [vendorAdmin])
 
-  useEffect(() => {   
-      console.log('orders', orders)
-  },[orders])
+  useEffect(() => {
+    console.log('orders', orders)
+  }, [orders])
 
   const { colors } = useAppTheme();
+  if (loading)
+    return (<AppPageWrapper>
+      <LoadingSpinner />
+    </AppPageWrapper>)
   return (
     <AppPageWrapper>
       <FlatList
@@ -46,9 +50,10 @@ const OrderContainer: React.FC = () => {
         extraData={orders}
         onEndReachedThreshold={0.1}
         //   ListFooterComponent={(moreproducts) ? <View row space="evenly" style={{ paddingVertical: 16 }}><View flex center><ActivityIndicator size="large" /></View></View> : null}
-        refreshing={false}
+        refreshing={loading}
         // onEndReached={() => fetchMore()}
         style={styles.articles}
+        showsVerticalScrollIndicator={false}
       />
     </AppPageWrapper>
   );
